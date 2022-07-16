cb6d8f5126e62859556d051088710aad271bbb11
joshua530 <joshuaomari5@gmail.com>
2022-07-16 09:04:11 +0300

    make betty compliant

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,45 @@
 #include "sort.h"
 #include <stdio.h>
 
-
 /**
  * insertion_sort_list - insertion sort implementation
- * 
+ *
  * @list: list to be sorted
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *iter, *tmp, *cur;
+	listint_t *iter, *tmp, *cur;
 
-    if (list == NULL || *list == NULL)
-        return;
+	if (list == NULL || *list == NULL)
+		return;
 
-    iter = *list;
+	iter = *list;
 
-    while (iter->next)
-    {
-        iter = iter->next;
-        tmp = iter;
+	while (iter->next)
+	{
+		iter = iter->next;
+		tmp = iter;
 
-        while (tmp->prev && tmp->prev->n > tmp->n)
-        {
-            cur = tmp->prev;
-            
-            if (cur->prev)
-            {
-                cur->prev->next = tmp;
-            }
-            tmp->prev = cur->prev;
-            cur->prev = tmp;
-            
-            if (tmp->next)
-            {
-                tmp->next->prev = cur;
-            }
-            cur->next = tmp->next;
-            tmp->next = cur;
+		while (tmp->prev && tmp->prev->n > tmp->n)
+		{
+			cur = tmp->prev;
 
-            if (!tmp->prev)
-                *list = tmp;
-        }
-    }
+			if (cur->prev)
+			{
+				cur->prev->next = tmp;
+			}
+			tmp->prev = cur->prev;
+			cur->prev = tmp;
+
+			if (tmp->next)
+			{
+				tmp->next->prev = cur;
+			}
+			cur->next = tmp->next;
+			tmp->next = cur;
+
+			if (!tmp->prev)
+				*list = tmp;
+		}
+	}
 }
